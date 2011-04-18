0f8dc1dd466446a029ebf725023e8d3e53f738e6
Rafael Cardoso <rafamvc@gmail.com>
2011-04-18 13:34:05 -0700

    commented validations to work with fields_for, added the reject on accepted_nested

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,7 +1,7 @@
 class Album < ActiveRecord::Base
   has_and_belongs_to_many :artists
   validates :name, :presence => true
-  has_many :musics
+  has_many :musics, :dependent => :destroy  
   
-  accepts_nested_attributes_for :musics
+  accepts_nested_attributes_for :musics, :reject_if => lambda { |a| a[:name].blank? && a[:mp3].blank? }
 end
